d8e0a18990e15818df529085edc2e8a3295b936d
João vitor de Oliveira Ferreira <89479485+Joao-vitor-090@users.noreply.github.com>
2023-05-24 19:02:22 -0300

    adição: exibição dos componentes do Game;

--- src/Componentes/Game.jsx
+++ src/Componentes/Game.jsx
@@ -1,30 +1,40 @@
-function Game(){
-    console.log("começo função")
+function Game() {
     const array = [];
-    
+  
     function getRandomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-      } 
-      const randomID = getRandomNumber(1,50);
-      for(let i = 0; i <= 6 ;i++){
-         const randomNumber = getRandomNumber(1, 50);
-         array[i] = randomNumber;
-      } 
-      for(let i = 0; i <= 0;i++){
-        let var1 ;
-        let randomNumber = getRandomNumber(1,randomID); /*randomNumber não pode cair com o mesmo valor que o randomID*/ 
-        var1 =  randomID - randomNumber ;
-        array.push(var1);
-        array.push(randomNumber);
-      }
-
-      return(
-        console.log("principal : " + randomID, "array: " + array +" ; " + " O tamanho do array é " + array.length + "." )
-        
-
-
-    
-    )
-}
-
-export default Game;
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+  
+    const randomID = getRandomNumber(1, 50);
+  
+    for (let i = 0; i < 6; i++) {
+      const randomNumber = getRandomNumber(1, 50);
+      array[i] = randomNumber;
+    }
+  
+    for (let i = 0; i < 2; i++) {
+      let var1;
+      let randomNumber = getRandomNumber(1, randomID);
+      var1 = randomID - randomNumber;
+      array.push(var1);
+      array.push(randomNumber);
+    }
+  
+    console.log(array);
+    console.log(array);
+    console.log(randomID);
+    console.log("resolução: " + array[8]  +"  "+  array[9] + " = " + randomID);
+    return (
+      <div className="main_game">
+        <h1>{randomID}</h1>
+        <div className="boxNumbers_game">
+          {array.map((number, index) => (
+            <li key={index} className="li_game">{number}</li>
+          ))}
+        </div>
+      </div>
+    );
+  }
+  
+  export default Game;
+  
